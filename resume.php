e6f4d2d0131846f1818a4a535ca29a0fce6ac0d8
aj0strow <alexander.ostrow@gmail.com>
2013-09-19 19:28:57 -0400

    updated resume

--- resume.php
+++ resume.php
@@ -20,59 +20,56 @@
 						Profile Summary
 					</h3>
 					<p>
-						Technical writer with ten years of experience in technical program management with a passion 
-						for writing technical content and developing educational materials.
+            Technical writer with ten years of experience in technical program management and a passion for writing
+            engineering documentation
 					</p>
 					
 					<h3>
 						Writing Skills
 					</h3>
+          
 					<ul>
-						<li>
-							Training materials for new engineering and management procedures
-						</li>
-						<li>
-							Engineering documents: requirements, design documents, test plans, manuals
-						</li>
-						<li>
-							Lesson plans for courses in math, physics, electronics
-						</li>
-						<li>
-							White papers
-						</li>
-						<li>
-							Technical materials adpated to non-technical audiences
-						</li>
-						<li>
-							Experience with the Chicago Style Manual
-						</li>
+            <li>User manuals and fact sheets</li>
+            <li>Training materials for new engineering and management procedures</li>
+            <li>Structured architecture, process, and test documentation</li>
+            <li>Process and program management flowcharts</li>
+            <li>Comprehensive editing and content organization</li>
+            <li>Engineering documents: requirements, design documents, test plans, manuals</li>
+            <li>White papers</li>
+            <li>Technical material adapted to non-technical audiences</li>
+            <li>Create, maintain, and adhere to corporate style guides</li>
+            <li>Experience with the <em>Chicago Manual of Style</em> and the <em>Microsoft Manual of Style for Technical 
+            Publications</em></li>
 					</ul>
 					
 					<h3>
 						Management Skills
 					</h3>
-					<ul>
-						<li>
-							Functional and project management
-						</li>
-						<li>
-							Schedules, budgets, project plans, risk analysis, risk abatement
-						</li>
-						<li>
-							Effective presentations
-						</li>
-					</ul>
+          <ul>
+            <li>Project management</li>
+            <li>Gather information from hardware engineers, software developers, product management, service 
+            engineers, and QA </li>
+            <li>Schedules, budgets, project plans, risk analysis, risk abatement</li>
+            <li>Manage simultaneous projects</li>
+            <li>Effective presentations</li>
+          </ul>
 					
 					<h3>
 						Computer Applications
 					</h3>
 					<p>
-						 Word, Project, Excel, PowerPoint, SharePoint, Visio, Illustrator
+						 Word, Project, Excel, PowerPoint, SharePoint, Visio, Illustrator, Final Draft, iMovie
 					</p>
+          
+          <h3>Programming Language Reading Competency</h3>
+          <p>
+            C, C++, C#, Fortran, Visual Basic
+          </p>
 					
 					<h3>
 						Education
 					</h3>
+          
 					<p>
 						Certificate in Technical Writing, University of Washington, 
 						Seattle, WA
@@ -89,162 +86,117 @@
 					<h3>
 						Professional History
 					</h3>
-					
-					<h4>
-						Independent Film Producer
-					</h4>
-					<p>
-						Self-employed independent contractor, Seattle WA (10/2003–12/2011)
-					</p>
-					<ul>
-						<li>
-							Managed production of corporate training videos and narrative films
-						</li>
-						<li>
-							Clients included HUD, FDA, Cornell University, and Boston University
-						</li>
-						<li>
-							Wrote scripts and press kits
-						</li>
-					</ul>
-					
-					<h4>
-						Launch Release Program Manager
-					</h4>
-					<p>
-						AT&T Wireless / Mobile, Redmond, WA (10/2003–12/2003), contract through 
-						Volt Technical Resources
-					</p>
-					<ul>
-						<li>
-							Managed online holiday advertisement campaign launches
-						</li>
-						<li>
-							Host sites included Yahoo and MSN
-						</li>
-						<li>
-							Converted 85% of projected store sales to web sales, exceeding the 
-							target goal by 15%
-						</li>
-						<li>
-							Wrote first advertisement launch procedure  
-						</li>
-					</ul>
-					
-					<h4>
-						Engineering Program Manager
-					</h4>
-					<p>
-						Siemens Medical, Issaquah, WA (2/2002–10/2002), contract through Volt 
-						Technical Resources
-					</p>
-					<ul>
-						<li>
-							Managed development of mid-range diagnostic ultrasound machines 
-						</li>
-						<li>
-							Educated stakeholders in development process 
-						</li>
-						<li>
-							Developed program management software applications to enable all 
-							departments to meet FDA documentation requirements for project planning 
-						</li>
-					</ul>
-					
-					<h4>
-						Software Program Manager
-					</h4>
-					<p>
-						Avenue A, Seattle WA (4/2001–7/2001)
-					</p>
-					<ul>
-						<li>
-							Managed backend advertisement server development projects 
-						</li>
-						<li>
-							Defined and prioritized development projects 
-						</li>
-						<li>
-							Wrote retroactive design document for advertisement server’s backend 
-							software 
-						</li>
-					</ul>
-					
-					<h4>
-						Software Program Manager
-					</h4>
-					<p>
-						Akamai, Seattle, WA (5/2000–1/2001)
-					</p>
-					<ul>
-						<li>
-							Wrote Functional Requirements Specifications for software releases 
-						</li>
-						<li>
-							Wrote regular status reports on software development and testing
-						</li>
-						<li>
-							Managed web broadcasting software projects 
-						</li>
-						<li>
-							Planned and scheduled software launches 
-						</li>
-						<li>
-							Used product to broadcast the National Democratic Convention, 8/2000
-						</li>
-					</ul>
-					
-					<h4>
-						Engineering Program Manager
-					</h4>
-					<p>
-						Philips Medical, Bothell, WA (3/1996–5/2000)
-					</p>
-					<ul>
-						<li>
-							Managed development of high-end diagnostic ultrasound machines
-						</li>
-						<li>
-							Trained technical leads in project management
-						</li>
-						<li>
-							Wrote the first set of Project Management Procedures for the 
-							Ultrasound Division 
-						</li>
-						<li>
-							Conducted three internal regulatory audits and wrote audit reports
-						</li>
-					</ul>
-					
-					<h4>
-						Acoustic Measurements Laboratory Supervisor
-					</h4>
-					<p>
-						Philips Medical, Bothell, WA (11/1993–3/1996)
-					</p>
-					<ul>
-						<li>
-							Directly supervised fifteen test technicians
-						</li>
-						<li>
-							Wrote first engineering project management procedure for Measurements 
-							Department  
-						</li>
-						<li>
-							Received the Truly Outstanding Performance Award for Process Improvements 
-							(1996)
-						</li>
-					</ul>
+          
+          <h4>Technical Writer/Process Engineer</h4>
+          <p>
+            EMC/Isilon Division, Seattle, WA (10/2012–7/2013), contract through Advantage Technical Resourcing
+          </p>
+          <ul>
+            <li>Developed troubleshooting flowcharts and knowledge based articles for developers and service 
+          engineers (Visio, Word, Excel)</li>
+            <li> Worked closely with subject matter experts, engineers, managers, supervisors, and end-users to 
+          identify topics, develop content, and test draft documents</li>
+            <li>Developed style guide for flowcharts</li>
+            <li>Managed troubleshooting documentation project</li>
+          </ul>
+          
+          <h4>Technical Writer</h4>
+          <p>
+            Open Data Center Alliance (ODCA), Seattle WA (9/2012–12/2012), contract through Make It Matter
+          </p>
+          <ul>
+            <li>Edited International Standards for Cloud service providers. Topics included: terminology, 
+          international service level agreements, audits, regulatory and business drivers</li>
+          </ul>
+          
+          <h4>Independent Film Producer</h4>
+          <p>Self-employed independent contractor, Seattle WA (10/2003–12/2011)</p>
+          <ul>
+            <li>Wrote press kits including promotional materials and marketing plans (Illustrator, Word, Corel Draw, 
+          iMovie, Illustrator)</li>
+            <li>Wrote scripts for training videos, advertisements, and narrative films (Word, Final Draft)</li>
+            <li>Managed production of corporate training videos and narrative films (Project, Excel)</li>
+            <li>Clients included HUD, FDA, Cornell University, and Boston University</li>
+          </ul>
+          
+          <h4>Launch Release Program Manager</h4>
+          <p>AT&amp;T Wireless / Mobile, Redmond, WA (10/2003–12/2003), contract through Volt Technical </p>
+          <ul>
+            <li>Interviewed engineers and graphic designers to gather information and wrote the first advertisement launch procedure (Word, Excel, Coral Draw)</li>
+            <li>Managed vendor relationship with Digitos (Project)</li>
+            <li>Managed online holiday advertisement campaign launches (Project, Excel)</li>
+            <li>Host sites included Yahoo and MSN</li>
+            <li>Converted 85% of projected store sales to web sales, exceeding the target goal by 15%</li>
+          </ul>
+          
+          <h4>Engineering Program Manager</h4>
+          <p>Siemens Medical, Issaquah, WA (2/2002–10/2002), contract through Volt Technical Resources</p>
+          <ul>
+            <li>Wrote reports for FDA submissions, program charters, and status reports for a variety of audiences including engineers, marketing, and management (Word, Excel)</li>
+            <li>Developed program management software applications to enable all departments to meet FDA documentation requirements for program planning (Word, Project, Excel)</li>
+            <li>Managed development of mid-range diagnostic ultrasound machines (Project, PowerPoint)</li>
+          </ul>
+          
+          <h4>Software Program Manager</h4>
+          <p>Avenue A, Seattle WA (4/2001–7/2001)</p>
+          <ul>
+            <li>Read software code written in C++ and interpreted the code into a design document for the advertisement server’s backend software (Word, Visio, Excel)</li>
+            <li>Wrote program charters and prioritized development projects (Word, Excel, Project)</li>
+            <li>Managed backend advertisement server development projects (Project)</li>
+            <li>Defined and prioritized development projects</li>
+          </ul>
+          
+          <h4>Software Program Manager</h4>
+          <p>Akamai, Seattle, WA (5/2000–1/2001)</p>
+          <ul>
+            <li>Used marketing requirements to write engineering specification documents for software releases (Word, Corel Draw, Excel)</li>
+            <li>Wrote regular status reports on software development and testing (Word, Excel, Project)</li>
+            <li>Wrote software test plans (Word, Excel)</li>
+            <li>Managed web broadcasting software projects (Project, PowerPoint)</li>
+            <li>Planned and scheduled software launches (Project)</li>
+            <li>Company used product to broadcast the National Democratic Convention, 8/2000</li>
+          </ul>
+          
+          <h4>Engineering Program Manager</h4>
+          <p>Philips Medical, Bothell, WA (3/1996–5/2000)</p>
+          <ul>
+            <li>Developed and wrote the first set of project management procedures for the Ultrasound Division (Word, Excel, Coral Draw)</li>
+            <li>Managed development of high-end diagnostic ultrasound machines (Project)</li>
+            <li>Trained technical leads in project management (PowerPoint)</li>
+            <li>Conducted three internal regulatory audits and wrote audit reports (Word)</li>
+          </ul>
+          
+          <h4>Acoustic Measurements Laboratory Supervisor</h4>
+          <p>Philips Medical, Bothell, WA (11/1993–3/1996)</p>
+          <ul>
+            <li>Wrote test procedures for ultrasound measurement technicians (Word, Excel)</li>
+            <li>Developed and wrote request documentation for customer use (Word, Excel)</li>
+            <li>Directly supervised fifteen test technicians (Project)</li>
+            <li>Wrote first engineering project management procedure for Measurements Department (Word, Project, Excel)</li>
+            <li>Received the Truly Outstanding Performance Award for Process Improvements (1996)</li>
+          </ul>
+          
+          <h4>Acoustic Measurements Engineer</h4>
+          <p>NTR Systems, Seattle, WA (4/1991–8/1993)</p>
+          <ul>
+            <li>Wrote first user manual for the company’s premiere produce, the Acoustic Intensity Measurement System, AIMS (Word, Excel)</li>
+            <li>Wrote educational materials to teach customers how to use AIMS to calibrate and measure equipment (Word, Excel)</li>
+            <li>Collected measurement data and wrote test reports for research scientists and FDA submissions (Word, Excel)</li>
+          </ul>
+
+          <hr />
 					
 					<h3>
 						Teaching Experience
 					</h3>
+          
 					<h4>
 						High School Math and Physics Teacher, 2 years
 					</h4>
 					<p>
 						United States Peace Corps, Naililili, Fiji 
 					</p>
+          
 					<h4>
 						College Electronics Teaching Fellow, 2 years
 					</h4>
